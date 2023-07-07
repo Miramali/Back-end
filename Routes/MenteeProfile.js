e9e665bf9e4c55c7a0970a9af22905a4ca5e3cf3
Hadeel-Ayman <haddelayman4@gmail.com>
2023-07-07 05:23:22 +0300

    add change

--- Routes/MenteeProfile.js
+++ Routes/MenteeProfile.js
@@ -6,11 +6,11 @@ const auth = require("../middleware/auth")
 
 
 
-router.post('/mentorProfile' ,auth, upload.single('avatar') , MenteeController.addNewMentee)
-router.get('/mentorProfile' , MenteeController.getAllMentee)
-router.get('/mentorProfile/:id' ,  MenteeController.getMentee)
-router.patch('/mentorProfile/:id' ,upload.single('avatar') ,  MenteeController.updateMentee)
-router.delete('/mentorProfile/:id' ,  MenteeController.removeMentee)
+router.post('/mentorProfile', auth, upload.single('avatar'), MenteeController.addNewMentee)
+router.get('/mentorProfile', MenteeController.getAllMentee)
+router.get('/mentorProfile/:id', MenteeController.getMentee)
+router.patch('/mentorProfile/:id', upload.single('avatar'), MenteeController.updateMentee)
+router.delete('/mentorProfile/:id', MenteeController.removeMentee)
 
 
 
--- Routes/SocialAuth.js
+++ Routes/SocialAuth.js
@@ -15,35 +15,35 @@ router.get(
 router.get(
   "/auth/google/callback",
   passport.authenticate("google",
-  { failureRedirect: "/login" },
-  (req, res) => {
-    const token = jwt.sign({ userId: req.user.id }, process.env.AUTH_KEY);
-    res.redirect(CLIENT_URL);
-  }
-));
+    { failureRedirect: "/login" },
+    (req, res) => {
+      const token = jwt.sign({ userId: req.user.id }, process.env.AUTH_KEY);
+      res.redirect(CLIENT_URL);
+    }
+  ));
 
 router.get("/auth/github", passport.authenticate("linkedin"));
 
 router.get(
   "/auth/github/callback",
   passport.authenticate("github",
-  { failureRedirect: "/login" },
-  (req, res) => {
-    const token = jwt.sign({ userId: req.user.id }, process.env.AUTH_KEY);
-    res.redirect(CLIENT_URL);
-  }
-));
+    { failureRedirect: "/login" },
+    (req, res) => {
+      const token = jwt.sign({ userId: req.user.id }, process.env.AUTH_KEY);
+      res.redirect(CLIENT_URL);
+    }
+  ));
 
 router.get("/auth/facebook", passport.authenticate("facebook"));
 
 router.get(
   "/auth/facebook/callback",
   passport.authenticate("facebook",
-  { failureRedirect: "/login" },
-  (req, res) => {
-    const token = jwt.sign({ userId: req.user.id }, process.env.AUTH_KEY);
-    res.redirect(CLIENT_URL);
-  }
-));
+    { failureRedirect: "/login" },
+    (req, res) => {
+      const token = jwt.sign({ userId: req.user.id }, process.env.AUTH_KEY);
+      res.redirect(CLIENT_URL);
+    }
+  ));
 
 module.exports = router;
