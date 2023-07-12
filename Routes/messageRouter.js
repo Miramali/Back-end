5c1efc8e6d47c675cdca351ebc1b3a74ad04e7a3
ran-sy <raneem.1999.ta@gmail.com>
2023-07-12 09:51:48 +0300

    comment update

--- Routes/messageRouter.js
+++ Routes/messageRouter.js
@@ -6,7 +6,7 @@ const router = express.Router();
 router.get("/receiver/:id", auth, messageController.getReceiverMessages);
 router.get("/sender/:id", auth, messageController.getSenderMessages);
 
-// /search for most relavent, /searchD for search by newest
+// /search for most relavent, /searchNewest for search by newest
 router.get("/search", auth, messageController.searchMessages);
 router.get("/searchNewest", auth, messageController.searchMessagesByDate);
 
